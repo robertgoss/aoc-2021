e915bc353962815d98ea769430ddc71b6de7f519
Robert Goss <goss.robert@gmail.com>
2021-12-03 22:05:17 +0000

    Add argument and do day 1 part 2 in haskell

--- haskell/app/Sonar.hs
+++ haskell/app/Sonar.hs
@@ -1,7 +1,13 @@
-module Sonar(numberIncreasing) where 
+module Sonar(numberIncreasing, numberIncreasingWindow) where 
+
+import Data.List(tails)
 
 numberIncreasing :: [Int] -> Int
 numberIncreasing (x:(y:rest)) 
    | x < y = 1 + numberIncreasing (y:rest)
    | otherwise = numberIncreasing (y:rest)
 numberIncreasing _ = 0
+
+numberIncreasingWindow :: [Int] -> Int
+numberIncreasingWindow = numberIncreasing . windows
+  where windows = map (sum . take 3) . tails
